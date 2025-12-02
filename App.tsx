938d19222069c673ecfea2771264c39e17135700
lifemateauto <lifemate.auto@gmail.com>
2025-12-02 15:16:44 +0800

    Fix wrong import path for processData

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
-import { processData } from "./utils/processData";
+import { processData } from "./utils/processor";
 import { ReportRow } from "./types";
 
 const App: React.FC = () => {
